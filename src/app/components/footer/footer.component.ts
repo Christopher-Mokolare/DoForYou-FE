561cffd79b010ac9ad7d41dc377f076ce6375d54
Obakeng Mokolare <obakeng@botsza.com>
2025-08-12 08:54:29 +0200

    feat: Add scrollToTop functionality to footer links for improved navigation experience

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink,Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -12,7 +12,17 @@ import { RouterLink } from '@angular/router';
 export class FooterComponent {
   isMenuCollapsed = true;
   currentYear = new Date().getFullYear();
+    constructor(private router: Router) {}
 
+ scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]).then(() => {
+      this.scrollToTop();
+    });
+  }
     postErrand() {
     window.open(
       'https://docs.google.com/forms/d/e/1FAIpQLSd_uoW_FP3Q3qTSZmDpsR1aqqXK35Os2EWCKJrKnQKoPUeTrg/viewform',
