86f4da63c0dff5fc7139545abc8f67bb2f2fcd11
OBAKENG Christopher-Mokolare <2co.mokolare@gmail.com>
2025-12-04 23:16:45 +0200

    feat: Update production environment to use Railway backend
    - Connect frontend to Railway backend URL
    - Update API and SignalR endpoints for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.doforyou.co.za',
+  apiUrl: 'https://doforyoubackend-production.up.railway.app',
   apiKey: 'DFY_63c6ee01-0ba6-49e1-9f67-4b752c523267',
   taskRequestForm: 'https://docs.google.com/forms/d/e/1FAIpQLSd_uoW_FP3Q3qTSZmDpsR1aqqXK35Os2EWCKJrKnQKoPUeTrg/viewform',
   whatsappNumber: '27795258611',
   appName: 'DoForYou',
   contactEmail: 'info@doforyou.co.za',
   supportPhone: '0795258611',
-  signalRUrl: 'https://api.doforyou.co.za/taskHub'
+  signalRUrl: 'https://doforyoubackend-production.up.railway.app/taskHub'
 };
