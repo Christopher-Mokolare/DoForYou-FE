6a21833590b9ce83643695502dd03465dea53a69
OBAKENG Christopher-Mokolare <2co.mokolare@gmail.com>
2026-08-01 02:57:55 +0200

    add Firebase hosting config, QA environment, and deploy workflow

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5001/api/v1',
+  apiUrl: 'https://qa-api.doforyou.co.za/api/v1',
   apiKey: 'DFY_QA_KEY',
 
   whatsappNumber: '27795258611',
   appName: 'DoForYou (QA)',
   contactEmail: 'qa@doforyou.co.za',
   supportPhone: '0795258611',
-  signalRUrl: 'http://localhost:5001/taskHub'
+  signalRUrl: 'https://qa-api.doforyou.co.za/taskHub'
 };
