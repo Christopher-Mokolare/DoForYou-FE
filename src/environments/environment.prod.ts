935ea9342efd19937bcae4f3e2a5a5a377abee7e
OBAKENG Christopher-Mokolare <2co.mokolare@gmail.com>
2026-08-01 03:16:26 +0200

    fix: update prod and qa environments to point to AWS APIs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:5001/api/v1',
+  apiUrl: 'https://api.doforyou.co.za/api/v1',
   apiKey: 'DFY_PROD_KEY',
 
   whatsappNumber: '27795258611',
   appName: 'DoForYou',
   contactEmail: 'info@doforyou.co.za',
   supportPhone: '0795258611',
-  signalRUrl: 'http://localhost:5001/taskHub'
+  signalRUrl: 'https://api.doforyou.co.za/taskHub'
 };
